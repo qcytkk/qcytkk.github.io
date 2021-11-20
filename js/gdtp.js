a9af42317f7c5c190104e312b22838526e129565
qcytkk <93875611+qcytkk@users.noreply.github.com>
2021-11-20 15:25:30 +0800

    Update gdtp.js

--- js/gdtp.js
+++ js/gdtp.js
@@ -9,10 +9,6 @@ $(function () {
         $("#a1").append($lia1);
     }
 
-    var stylektt = document.styleSheets[0];
-    stylektt.insertRule("@keyframes diva0 {100% {transform: translateX(300px);}}");
-    stylektt.insertRule("@keyframes diva1 {100% {transform: translateX(-300px);}}");
-
     $(window).resize(function () {
         var windoww = $(window).width();
         var lix = parseInt(windoww / 150);
